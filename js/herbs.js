91a66d25349ec4cf7e6bf22645148137da8a4f01
Evan Dudla <evan@dud.la>
2015-09-10 15:57:53 +0700

    Mobile herbs stylesheet changes, optimized images for Hong Hua, Dang Shen, and Bai Shao

--- js/herbs.js
+++ js/herbs.js
@@ -1,53 +1,53 @@
 var herb_data = [
   {'name':'hong hua',
    'desc':'hong hua is used to regulate menstruation for those with amenorrhea and dysmenorrhea',
-    'url':'/image/herb_full/HongHua__004.jpg'},
+    'filename':'HongHua__004.jpg'},
   {'name':'dang shen',
    'desc':'dang shen is used to treat fatigue, poor appetite, and nausea',
-    'url':'/image/herb_full/$3.60__001.jpg'},
+    'filename':'$3.60__001.jpg'},
   {'name':'bai shao',
    'desc':'bai shao can treat abnormal menstruation and can also help alleviate headaches and migraines',
-    'url':'/image/herb_full/$2.52__008.jpg'},
+    'filename':'$2.52__008.jpg'},
   {'name':'du zhong',
    'desc':'du zhong treats lower back, knee, and leg pain',
-    'url':''},
+    'filename':''},
   {'name':'xin yi hua',
    'desc':'xin yi hua is great for the common cold with nasal congestion',
-    'url':''},
+    'filename':''},
   {'name':'ji xue teng',
    'desc':'ji xue teng treats tight muscles and sore joints',
-    'url':''},
+    'filename':''},
   {'name':'gou qi zi',
    'desc':'gou qi zi can help improve visual acuity',
-    'url':''},
+    'filename':''},
   {'name':'fu ling',
    'desc':'fu ling can improve appetite, headache, dizziness, and bloating',
-    'url':''},
+    'filename':''},
 
   {'name':'huang qi',
    'desc':'huang qi treats fatigue, shortness of breath, and diarrhea',
-    'url':''},
+    'filename':''},
   {'name':'qing hao',
    'desc':'qing hao is used to relieve fever, headache, and congestion',
-    'url':''},
+    'filename':''},
   {'name':'ju hua',
    'desc':'ju hua can relieve fever, sore throat, and dry eyes',
-    'url':''},
+    'filename':''},
   {'name':'chuan xiong',
    'desc':'chuan xiong is popular for gynecological problems',
-    'url':''},
+    'filename':''},
   {'name':'sheng di huang',
    'desc':'sheng di huang relieves fever, anxiety, and night sweats',
-    'url':''},
+    'filename':''},
   {'name':'gan cao',
    'desc':'gan cao can help alleviate fatigue, loose stools, and low appetite',
-    'url':''},
+    'filename':''},
   {'name':'xu duan',
    'desc':'xu duan is commonly used for lower back and knee pain',
-    'url':''},
+    'filename':''},
   {'name':'lian zi',
    'desc':'lian zi helps treat low appetite, bloating, and indigestion',
-    'url':''},
+    'filename':''},
 ];
 
 
@@ -56,13 +56,15 @@ $(function() {
 });
 
 
-function herb_thumb_click(e){
+function herb_thumb_click(e) {
+  var isMobile = $('#menuButtonTop').is(':visible');
+
   $('div.herb_thumb').removeClass('active');
   $(this).addClass('active');
- 
+
   var idx = $(this).index();  // 0-15
 
-  $('#content').css('background-image','url('+herb_data[idx]['url']+')');
+  $('#content').css('background-image','url(' + '/image/herb_' + (isMobile ? 'mobile' : 'full' ) + '/' + herb_data[idx]['filename'] + ')');
   $('#rightsquare .title').html(herb_data[idx]['name']);
   $('#rightsquare .content').html(herb_data[idx]['desc'].replace(herb_data[idx]['name'],'<i>'+herb_data[idx]['name']+'</i>'));
 }
