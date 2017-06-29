ee8f93912c2a5bf9e3eae38f95fc80092b91fdb1
Sai Jurawanichkul <me@sai-ju.com>
2017-06-28 21:30:16 -0700

    Updated Book Now button to Gen Books

--- header.php
+++ header.php
@@ -102,7 +102,7 @@ mixpanel.init("e2ed76531266dfb38fcd7b8bf25ee504");</script><!-- end Mixpanel -->
         </div>
       </div>
       <div id="navButton">
-        <a id="book_button" class="btn btn-small" href="https://clients.mindbodyonline.com/classic/ws?studioid=304788&stype=-98" target="_blank">Book&nbsp;Now</a>
+        <a id="book_button" class="btn btn-small" href="https://www.genbook.com/bookings/slot/reservation/30397544?bookingSourceId=1000" target="_blank">Book&nbsp;Now</a>
 				<span class="check-insurance">
 					<a id="ins_button" class="btn btn-small" href="https://docs.google.com/forms/d/1dKcsHZYOlA6yIAVjkly3l6j2yrhDAvflk00PtshKAxE/viewform" target="_blank">Check Insurance</a>
 				</span>
