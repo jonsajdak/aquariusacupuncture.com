f672f6ccd04a869a28ebe80b33763654173a6323
jonsajdak <jon.sajdak@gmail.com>
2017-11-27 18:24:13 -0500

    beauty acupuncture addition

--- beauty.php
+++ beauty.php
@@ -2,40 +2,11 @@
 
    <link rel="stylesheet" href="/css/beauty_mobile.css">
 
-   <div id="content" class="about">
+   <div id="content" class="beauty">
       <div id="rightsquare">
-        <div class="title">Dr. Sarah Emily Sajdak</div>
-        <div class="subtitle">DAOM, L.Ac</div>
+        <div class="title">Beauty Acupuncture™</div>
         <div class="content">
-   Sarah is an intuitive healer who uses traditional Chinese medicine to treat the underlying root causes of disease pattern and disharmony in the body. Sarah's treatments strengthen the system so that the body can naturally return to a state of balance. This way, as ailments are relieved, signs and symptoms do not return, and the patient will be empowered to maintain his or her wellbeing and overall health. Traditional Chinese medicine is unique in its ability to bring success to any treatment plan, whether through primary or complementary healthcare. Sarah's particular interests include internal medicine, emotional disorders, women's health, pain management, migraines, and skin conditions. She has experience treating patients in a variety of clinical settings, including community acupuncture centers, doctors’ offices, and hospitals. 
-<br/><br/>
-Sarah holds her doctorate in Chinese Medicine, as well as a specialized diploma in dermatology. She received her master’s degree in Traditional Oriental Medicine (MSTOM) at the Pacific College of Oriental College. She is nationally board certified (NCCAOM) in traditional Oriental medicine, acupuncture, and Chinese herbal medicine, and holds state licensure in acupuncture in California and New York.
-<!--
-          <div class="section">
-            <b>Schools Attended:</b>
-            <dd>Pacific College of Oriental Medicine</dd>
-            <dd>New York University
-            <dd>Universite Paris VII
-          </div>
-          <div class="section">
-            <b>Modalities</b>
-            <dd>Acupuncture
-            <dd>Herbs
-            <dd>Cupping
-            <dd>Moxibustion
-            <dd>Tui Na
-            <dd>& Other Traditional Modalities
-          </div>
-          <div class="section">
-            <b>Areas of Interest:</b>
-            <dd>Internal Medicine
-            <dd>Emotional Disorders
-            <dd>Pain Management
-            <dd>Women's Health
-            <dd>Fertility
-            <dd>Facial Rejuvination & More...
-          </div>
--->
+Sajdak is the founder and originator of Beauty Acupuncture™. Using holistic methods Sajdak works with actors, models and celebrities for non surgical, natural, and organic beauty solutions. Sajdak's Beauty Acupuncture™ method is a holistic remedy that reduces lines, acne, dryness, puffiness, under eye circles, and evens out skin tone and restores skin's natural glow. 
         </div>
       </div>
    </div>
