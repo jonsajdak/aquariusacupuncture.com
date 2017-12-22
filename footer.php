b7719aff487ce8007812454b6ade40997ffd86e1
jonsajdak <jon.sajdak@gmail.com>
2017-12-21 22:29:10 -0500

    updated address on footer for mobile

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
         </span>
       </div>
       <div id="navAddressBottom">
-       80 E 11th Street, Suite 309
-       <br/>New York, NY 10003
-       <br/>(917) 496-5481
+        928 Broadway, Suite 703
+        <br/>New York, NY 10010
+        <br/>(917) 496-5481
       </div>
       <div id="navSocialIconsBottom">
           <a href='m&#97;ilt&#111;&#58;aq&#117;&#97;%72&#105;u&#115;acupu&#110;&#99;%7&#52;ure&#64;gm&#97;&#105;l&#46;%63%6Fm'><img src="image/email.png"/></a>
@@ -16,7 +16,7 @@
           <a href="https://instagram.com/aquariusacupuncture/"><img src="image/instagram.png"/></a>
       </div>
       <div id="navCopyrightBottom">
-       &copy; 2015 Aquarius Acupuncture PLLC
+       &copy; <?= date("Y") ?> Aquarius Acupuncture PLLC
       </div>
 
 <?php
