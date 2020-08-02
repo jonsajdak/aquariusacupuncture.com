ffd301fb574abc25a7621aeaeec4b0573ecee5a4
jonsajdak <jon.sajdak@gmail.com>
2020-08-02 13:20:22 -0400

    about update

--- about.php
+++ about.php
@@ -7,11 +7,64 @@
         <div class="title">Dr. Sarah Emily Sajdak</div>
         <div class="subtitle">DAOM, L.Ac</div>
         <div class="content">
+          <div style="font-size:26px">Somehow, my path has led me to become the Chinese Medicine practitioner for artists, musicians and creative types (who want to fix their acne, migraines and fertility issues)</div>
+<br/><br/>
+That’s not how I initially planned my business, but I guess when you’re an artist and musician yourself, you tend to attract like-minded people (and I love it). 
+<br/><br/>
+Hi, I’m Dr. Sarah Emily Sajdak (DAOM, L.Ac) and I think there’s something about us creatives that attracts us to alternative medicine. We don’t like to be limited by tradition and other people’s systems don’t typically work for us. We’re always forging our own paths. And the Traditional Chinese Medicine holistic approach to health is very much about finding the unique path to health that works for you.
+<br/><br/>
+There is no “one-size-fits-all” prescription with what I do.
+When we have our sessions together, we’ll get into the details that traditional Western medicine doctors will never ask about. My questions might seem mundane at first, but I like to dig into the minutiae to see what may have been missed. Very often, it’s a tiny detail that connects the dots and shows the way to heal. 
+<br/><br/>
+If you’re wondering what I mean, stick out your tongue! (and check the results with my Tongue Diagnostic Guide). You may find some surprising answers - and if you find more questions, we can talk about it on a free 10-minute call! 
+<br/><br/>
+Yeah, Traditional Chinese Medicine is different. It takes the whole you - your body, your lifestyle, your eating habits, sleeping habits and stress into account. In fact, the first question I’ll ask you is “Do you like your job?” because that alone can deeply affect your health.
+<br/><br/>
+A lesser known aspect of Traditional Chinese Medicine is working with energetic impressions - energy work, or balancing spiritual and physical disharmony. So I also observe things like how you move your body, the cadence of your voice, the shade of your skin, the color of your eyes, where your body stores weight — all of those and more tell me what may be out of balance.
+<br/><br/>
+Then we’ll work together to bring you back into balance, so you’ll feel like the best version of yourself. 
+<br/><br/>
+My specialties include healing migraines, acne and fertility issues using a blend of:
+<br/>Acupuncture
+<br/>Herbs, supplements, and diet - the “food as medicine” approach
+<br/>Mindfulness & Mindset to reduce the stress that can block healing from happening
+<br/><br/>
+What is it like to have a session with me?
+<br/><br/>
+A lot of my clients tell me they feel profound, unconditional love when they leave my sessions that they carry into the rest of their weeks — that’s a side effect, I guess? Others tell me it’s like speaking to your intuitive best friend, who knows a lot about different ways to heal naturally. 
+<br/><br/>
+In my coaching sessions (Traditional Chinese Medicine minus the in-person acupuncture), I will ask you questions to get to know the whole you. From there I will make recommendations of herbs, supplements and realistically doable lifestyle changes (ie. I’m not going to tell you to move from New York to a monastery, or change your high-stress acting job to rural chicken farmer). We will try things and see how you feel, moving towards what makes you feel better. 
+<br/><br/>
+And in the process, you’ll find that…
+<br/>Your migraines decrease in frequency and severity (I used to have terrible migraines — they led me to Traditional Chinese Medicine!)
+<br/>Your skin clears and brightens, and you’ll notice that lines, acne, dryness, puffiness, and under-eye circles reduce, and skin tone evens out.
+<br/>Your natural rhythms and fertility come back into alignment (because your whole self comes back into alignment).
+<br/><br/>
+Your body wants to be in balance. What I love about Traditional Chinese Medicine is that it uses every single tool to help you get there. You won’t just fix your health issue. You’ll change your life, and love every second. 
+<br/><br/>
+Let’s work together
+<br/><br/>
+Things I’m into:
+<br/>Breaking rules and questioning stories. So many of us put limits on ourselves that don’t need to be there. I’m all about throwing out what doesn’t work, and building a new system just for you.
+<br/>Lions Mane and Reishi mushrooms are amazing for mood, mental clarity and energy.
+<br/>Not sticking your tongue out in photos. Once you know what you’re looking for, it’s way too personal (your tongue tells your whole life story!).
+<br/>Watercolor painting, drawing, clarinet, piano, ukulele (hey, I told you I was a musician!). 
+<br/><br/>
+My qualifications:
+<br/>I hold a doctorate in Traditional Chinese Medicine and certificates in dermatology and fertility.
+<br/>I have a Master’s degree in Traditional Oriental Medicine (MSTOM) at the Pacific College of Oriental Medicine. 
+<br/>I’m nationally board certified (NCCAOM) in Traditional Oriental Medicine, acupuncture and Chinese herbal medicine.
+<br/>I hold state licensures for acupuncture in California and New York.
+<br/><br/>
+
+
+<!--
 Dr. Sarah Emily Sajdak is an intuitive healer who uses traditional Chinese medicine to treat the underlying root causes of disease pattern and disharmony in the body. Sajdak is the founder and originator of Beauty Acupuncture. Using holistic methods Sajdak works with actors, models and celebrities for non surgical, natural, and organic beauty solutions. Sajdak's Beauty Acupuncture method is a holistic remedy that reduces lines, acne, dryness, puffiness, under eye circles, and evens out skin tone and restores skin's natural glow. 
 <br/><br/>
 Sajdak's treatments strengthen the system so that the body can naturally return to a state of balance. This way, as ailments are relieved, signs and symptoms do not return, and the patient will be empowered to maintain his or her wellbeing and overall health. Traditional Chinese medicine is unique in its ability to bring success to any treatment plan, whether through primary or complementary healthcare. Sarah's particular interests include internal medicine, emotional disorders, women's health, pain management, migraines, and skin conditions. Sajdak has experience treating patients in a variety of clinical settings, including community acupuncture centers, doctors' offices, and hospitals. 
 </br></br>
 Sajdak holds her doctorate in Chinese Medicine, as well as a specialized diploma in dermatology. She received her master's degree in Traditional Oriental Medicine (MSTOM) at the Pacific College of Oriental College. She is nationally board certified (NCCAOM) in traditional Oriental medicine, acupuncture, and Chinese herbal medicine, and holds state licensure in acupuncture in California and New York.
+-->
 <!--
           <div class="section">
             <b>Schools Attended:</b>
