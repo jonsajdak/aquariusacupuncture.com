addef75e1fb6f63c3a201eee01d5c7168908746f
jonsajdak <jon.sajdak@gmail.com>
2020-08-02 13:35:35 -0400

    about update

--- about.php
+++ about.php
@@ -13,10 +13,10 @@ That’s not how I initially planned my business, but I guess when you’re an a
 <br/><br/>
 Hi, I’m Dr. Sarah Emily Sajdak (DAOM, L.Ac) and I think there’s something about us creatives that attracts us to alternative medicine. We don’t like to be limited by tradition and other people’s systems don’t typically work for us. We’re always forging our own paths. And the Traditional Chinese Medicine holistic approach to health is very much about finding the unique path to health that works for you.
 <br/><br/>
-There is no “one-size-fits-all” prescription with what I do.
-When we have our sessions together, we’ll get into the details that traditional Western medicine doctors will never ask about. My questions might seem mundane at first, but I like to dig into the minutiae to see what may have been missed. Very often, it’s a tiny detail that connects the dots and shows the way to heal. 
+<div style="font-size:15px;font-color:grey;">There is no “one-size-fits-all” prescription with what I do.</div>
+<br/><br/>When we have our sessions together, we’ll get into the details that traditional Western medicine doctors will never ask about. My questions might seem mundane at first, but I like to dig into the minutiae to see what may have been missed. Very often, it’s a tiny detail that connects the dots and shows the way to heal. 
 <br/><br/>
-If you’re wondering what I mean, stick out your tongue! (and check the results with my Tongue Diagnostic Guide). You may find some surprising answers - and if you find more questions, we can talk about it on a free 10-minute call! 
+<dd><b>If you’re wondering what I mean</b>, stick out your tongue! (and check the results with my Tongue Diagnostic Guide). You may find some surprising answers - and if you find more questions, we can talk about it on a free 10-minute call! 
 <br/><br/>
 Yeah, Traditional Chinese Medicine is different. It takes the whole you - your body, your lifestyle, your eating habits, sleeping habits and stress into account. In fact, the first question I’ll ask you is “Do you like your job?” because that alone can deeply affect your health.
 <br/><br/>
@@ -29,13 +29,13 @@ My specialties include healing migraines, acne and fertility issues using a blen
 <br/>• Herbs, supplements, and diet - the “food as medicine” approach
 <br/>• Mindfulness & Mindset to reduce the stress that can block healing from happening
 <br/><br/>
-What is it like to have a session with me?
+<div style="font-size:16px;">What is it like to have a session with me?</div>
 <br/><br/>
 A lot of my clients tell me they feel profound, unconditional love when they leave my sessions that they carry into the rest of their weeks — that’s a side effect, I guess? Others tell me it’s like speaking to your intuitive best friend, who knows a lot about different ways to heal naturally. 
 <br/><br/>
 In my coaching sessions (Traditional Chinese Medicine minus the in-person acupuncture), I will ask you questions to get to know the whole you. From there I will make recommendations of herbs, supplements and realistically doable lifestyle changes (ie. I’m not going to tell you to move from New York to a monastery, or change your high-stress acting job to rural chicken farmer). We will try things and see how you feel, moving towards what makes you feel better. 
 <br/><br/>
-And in the process, you’ll find that…
+<b>And in the process, you’ll find that…</b>
 <br/>Your migraines decrease in frequency and severity (I used to have terrible migraines — they led me to Traditional Chinese Medicine!)
 <br/>Your skin clears and brightens, and you’ll notice that lines, acne, dryness, puffiness, and under-eye circles reduce, and skin tone evens out.
 <br/>Your natural rhythms and fertility come back into alignment (because your whole self comes back into alignment).
@@ -45,10 +45,10 @@ Your body wants to be in balance. What I love about Traditional Chinese Medicine
 Let’s work together
 <br/><br/>
 Things I’m into:
-<br/>• Breaking rules and questioning stories. So many of us put limits on ourselves that don’t need to be there. I’m all about throwing out what doesn’t work, and building a new system just for you.
-<br/>• Lions Mane and Reishi mushrooms are amazing for mood, mental clarity and energy.
-<br/>• Not sticking your tongue out in photos. Once you know what you’re looking for, it’s way too personal (your tongue tells your whole life story!).
-<br/>• Watercolor painting, drawing, clarinet, piano, ukulele (hey, I told you I was a musician!). 
+<br/>• <b>Breaking rules and questioning stories.</b> So many of us put limits on ourselves that don’t need to be there. I’m all about throwing out what doesn’t work, and building a new system just for you.
+<br/>• <b>Lions Mane and Reishi mushrooms</b> are amazing for mood, mental clarity and energy.
+<br/>• <b>Not sticking your tongue out in photos.</b> Once you know what you’re looking for, it’s way too personal (your tongue tells your whole life story!).
+<br/>• <b>Watercolor painting, drawing, clarinet, piano, ukulele</b> (hey, I told you I was a musician!). 
 <br/><br/>
 My qualifications:
 <br/>• I hold a doctorate in Traditional Chinese Medicine and certificates in dermatology and fertility.
